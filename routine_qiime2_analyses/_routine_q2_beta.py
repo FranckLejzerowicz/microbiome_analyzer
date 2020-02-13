82e174ba4bb18201be5168e8e968e7d0ef9528ba
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-13 12:26:03 -0800

    improve get metrics and make command strings not lists

--- routine_qiime2_analyses/_routine_q2_beta.py
+++ routine_qiime2_analyses/_routine_q2_beta.py
@@ -10,13 +10,14 @@ import os
 from os.path import basename, dirname, isdir, isfile, splitext
 
 from routine_qiime2_analyses._routine_q2_xpbs import xpbs_call
-from routine_qiime2_analyses._routine_q2_io_utils import get_job_folder, get_analysis_folder
+from routine_qiime2_analyses._routine_q2_io_utils import get_metrics, get_job_folder, get_analysis_folder, run_export
 
 
-def run_beta(i_datasets_folder: str, datasets: dict, datasets_features:dict, beta_metrics: list,
-             wol_trees: dict, force: bool, prjct_nm: str, qiime_env: str) -> dict:
+def run_beta(i_datasets_folder: str, datasets: dict, trees: dict,
+             force: bool, prjct_nm: str, qiime_env: str) -> dict:
 
-    print('# Calculate beta diversity indices')
+    beta_metrics = get_metrics('beta_metrics')
+
     job_folder = get_job_folder(i_datasets_folder, 'beta')
     job_folder2 = get_job_folder(i_datasets_folder, 'beta/chunks')
 
@@ -24,57 +25,54 @@ def run_beta(i_datasets_folder: str, datasets: dict, datasets_features:dict, bet
     written = 0
     run_pbs = '%s/2_run_beta.sh' % job_folder
     with open(run_pbs, 'w') as o:
-        for dataset, files_lists in datasets.items():
+        for dataset, tsv_meta_pds in datasets.items():
+            tsv, meta = tsv_meta_pds
             if dataset not in betas:
                 betas[dataset] = {}
             out_sh = '%s/run_beta_%s.sh' % (job_folder2, dataset)
             out_pbs = '%s.pbs' % splitext(out_sh)[0]
             with open(out_sh, 'w') as sh:
-                for files_list in files_lists:
-                    qza = files_list[0].replace('.tsv', '.qza')
-                    meta = files_list[1]
-                    divs = []
-                    for metric in beta_metrics:
-                        odir = get_analysis_folder(i_datasets_folder, 'beta/%s' % dataset)
-                        out_fp = '%s/%s_%s_DM.qza' % (odir, basename(splitext(qza)[0]), metric)
-                        if force or not os.path.isfile(out_fp):
-                            if 'unifrac' in metric:
-                                if dataset not in datasets_features:
-                                    continue
-                                cmd = [
-                                    'qiime', 'diversity', 'beta-phylogenetic',
-                                    '--i-table', qza,
-                                    '--i-phylogeny', wol_trees[dataset],
-                                    '--p-metric', metric,
-                                    '--p-n-jobs', '1',
-                                    '--o-distance-matrix', out_fp]
+                qza = tsv.replace('.tsv', '.qza')
+                divs = []
+                for metric in beta_metrics:
+                    odir = get_analysis_folder(i_datasets_folder, 'beta/%s' % dataset)
+                    out_fp = '%s/%s_%s_DM.qza' % (odir, basename(splitext(qza)[0]), metric)
+                    if force or not os.path.isfile(out_fp):
+                        if 'unifrac' in metric:
+                            if dataset not in trees:
+                                continue
+                            cmd = 'qiime diversity beta-phylogenetic \\ \n'
+                            if trees[dataset][0]:
+                                cmd += '--i-table %s \\ \n' % trees[dataset][0]
                             else:
-                                cmd = [
-                                    'qiime', 'diversity', 'beta',
-                                    '--i-table', qza,
-                                    '--p-metric', metric,
-                                    '--p-n-jobs', '1',
-                                    '--o-distance-matrix', out_fp]
-                            sh.write('echo "%s"\n' % ' '.join(cmd))
-                            sh.write('%s\n' % ' '.join(cmd))
-                            written += 1
-                        divs.append(out_fp)
-                    betas[dataset][meta] = divs
+                                cmd += '--i-table %s \\ \n' % qza
+                            cmd += '--i-phylogeny %s \\ \n' % trees[dataset][1]
+                        else:
+                            cmd = 'qiime diversity beta \\ \n'
+                            cmd += '--i-table %s \\ \n' % qza
+                        cmd += '--p-metric %s \\ \n' % metric
+                        cmd += '--p-n-jobs 1 \\ \n'
+                        cmd += '--o-distance-matrix %s\n' % out_fp
+                        sh.write('echo "%s"\n' % ' '.join(cmd))
+                        sh.write('%s\n' % ' '.join(cmd))
+                        written += 1
+                    divs.append(out_fp)
+                betas[dataset][meta] = divs
             if written:
-                xpbs_call(out_sh, out_pbs, '%s.mg.bt.%s' % (prjct_nm, dataset), qiime_env,
+                xpbs_call(out_sh, out_pbs, '%s.bt.%s' % (prjct_nm, dataset), qiime_env,
                           '24', '1', '1', '10', 'gb')
                 o.write('qsub %s\n' % out_pbs)
             else:
-                print('\nNothing written in', out_sh, '--> removed')
                 os.remove(out_sh)
-    print('[TO RUN] sh', run_pbs)
+    if written:
+        print('# Calculate beta diversity indices')
+        print('[TO RUN] sh', run_pbs)
     return betas
 
 
 def export_beta(i_datasets_folder: str, betas: dict,
                 force: bool, prjct_nm: str, qiime_env: str) -> None:
 
-    print('# Export beta diversity matrices')
     job_folder = get_job_folder(i_datasets_folder, 'beta')
     out_sh = '%s/2x_run_beta_export.sh' % job_folder
     out_pbs = '%s.pbs' % splitext(out_sh)[0]
@@ -83,26 +81,24 @@ def export_beta(i_datasets_folder: str, betas: dict,
         for dat, meta_mats in betas.items():
             for meta, mats in meta_mats.items():
                 for mat in mats:
-                    out_dir = splitext(mat)[0]
-                    mat_export = '%s.tsv' % out_dir
+                    mat_export = '%s.tsv' % splitext(mat)[0]
                     if force or not isfile(mat_export):
-                        sh.write('\nqiime tools export --input-path %s --output-path %s\n' % (mat, out_dir))
-                        sh.write('mv %s/distance-matrix.tsv %s\n' % (out_dir, mat_export))
-                        sh.write('rm -rf %s\n' % out_dir)
+                        cmd = run_export(mat, mat_export, '')
+                        sh.write('echo "%s"\n' % cmd)
+                        sh.write('%s\n\n' % cmd)
                         written += 1
     if written:
         xpbs_call(out_sh, out_pbs, '%s.xprt.bt' % prjct_nm, qiime_env,
                   '2', '1', '1', '1', 'gb')
+        print('# Export beta diversity matrices')
         print('[TO RUN] qsub', out_pbs)
     else:
-        print('\nNothing written in', out_sh, '--> removed')
         os.remove(out_sh)
 
 
 def run_pcoas(i_datasets_folder: str, betas: dict,
               force: bool, prjct_nm: str, qiime_env: str) -> dict:
 
-    print('# Calculate principal coordinates')
     job_folder = get_job_folder(i_datasets_folder, 'pcoa')
     job_folder2 = get_job_folder(i_datasets_folder, 'pcoa/chunks')
     odir = get_analysis_folder(i_datasets_folder, 'pcoa')
@@ -124,21 +120,20 @@ def run_pcoas(i_datasets_folder: str, betas: dict,
                             pcoas_d[dataset].setdefault(meta, []).append(out_pcoa)
                             if not isdir(dirname(out_pcoa)):
                                 os.makedirs(dirname(out_pcoa))
-                            cmd = [
-                                'qiime', 'diversity', 'pcoa',
-                                '--i-distance-matrix', DM,
-                                '--o-pcoa', out_pcoa]
-                            sh.write('echo "%s"\n' % ' '.join(cmd))
-                            sh.write('%s\n' % ' '.join(cmd))
+                            cmd = 'qiime diversity pcoa \\ \n'
+                            cmd += '--i-distance-matrix %s \\ \n' % DM
+                            cmd += '--o-pcoa %s\n' % out_pcoa
+                            sh.write('echo "%s"\n' % cmd)
+                            sh.write('%s\n\n' % cmd)
                             written += 1
             if written:
                 xpbs_call(out_sh, out_pbs, '%s.pc.%s' % (prjct_nm, dataset), qiime_env,
                           '10', '1', '2', '2', 'gb')
                 o.write('qsub %s\n' % out_pbs)
             else:
-                print('\nNothing written in', out_sh, '--> removed')
                 os.remove(out_sh)
     if written:
+        print('# Calculate principal coordinates')
         print('[TO RUN] sh', run_pbs)
     return pcoas_d
 
@@ -146,11 +141,11 @@ def run_pcoas(i_datasets_folder: str, betas: dict,
 def run_emperor(i_datasets_folder: str, pcoas_d: dict,
                 prjct_nm: str, qiime_env: str) -> None:
 
-    print('# Make EMPeror plots')
     job_folder = get_job_folder(i_datasets_folder, 'emperor')
     job_folder2 = get_job_folder(i_datasets_folder, 'emperor/chunks')
     odir = get_analysis_folder(i_datasets_folder, 'emperor')
 
+    written = 0
     first_print = 0
     run_pbs = '%s/4_run_emperor.sh' % job_folder
     with open(run_pbs, 'w') as o:
@@ -166,22 +161,31 @@ def run_emperor(i_datasets_folder: str, pcoas_d: dict,
                 else:
                     meta = meta_alphas
                 for pcoa in pcoas:
-                    out_sh = '%s/run_emperor_%s_%s.sh' % (job_folder2, dataset, basename(pcoa.split('_DM_')[0]))
+                    out_sh = '%s/run_emperor_%s_%s.sh' % (
+                        job_folder2, dataset, basename(pcoa.split('_DM_')[0]))
                     out_pbs = '%s.pbs' % splitext(out_sh)[0]
                     with open(out_sh, 'w') as sh:
                         out_plot = '%s_emperor.qzv' % splitext(pcoa)[0].replace('/pcoa/', '/emperor/')
                         if not isdir(dirname(out_plot)):
                             os.makedirs(dirname(out_plot))
-                        emp_cmd = [
-                            'qiime', 'emperor', 'plot',
-                            '--m-metadata-file', meta,
-                            '--i-pcoa', pcoa,
-                            '--o-visualization', out_plot]
-                        sh.write('echo "%s"\n' % ' '.join(emp_cmd))
-                        sh.write('%s\n' % ' '.join(emp_cmd))
+                        cmd = 'qiime emperor plot \\ \n'
+                        cmd += '--m-metadata-file %s \\ \n' % meta
+                        cmd += '--i-pcoa %s \\ \n' % pcoa
+                        cmd += '--o-visualization %s\n' % out_plot
+                        sh.write('echo "%s"\n' % cmd)
+                        sh.write('%s\n\n' % cmd)
+                        written += 1
 
-                    xpbs_call(out_sh, out_pbs,
-                              '%s.mprr%s-%s' % (prjct_nm, dataset, basename(pcoa.split('_DM_')[0])), qiime_env,
-                              '10', '1', '1', '2', 'gb')
-                    o.write('qsub %s\n' % out_pbs)
-    print('[TO RUN] sh', run_pbs)
+                    if written:
+                        xpbs_call(out_sh, out_pbs,
+                                  '%s.mprr%s-%s' % (
+                                      prjct_nm, dataset,
+                                      basename(pcoa.split('_DM_')[0])),
+                                  qiime_env,
+                                  '10', '1', '1', '2', 'gb')
+                        o.write('qsub %s\n' % out_pbs)
+                    else:
+                        os.remove(out_sh)
+    if written:
+        print('# Make EMPeror plots')
+        print('[TO RUN] sh', run_pbs)
