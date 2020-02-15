8a1c4efebae27c3a189262a79a1e0183f761a4c9
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-14 18:08:15 -0800

    make less chunks for deicode (one per dataset)

--- routine_qiime2_analyses/_routine_q2_deicode.py
+++ routine_qiime2_analyses/_routine_q2_deicode.py
@@ -21,61 +21,70 @@ def run_deicode(i_folder: str, datasets: dict, p_perm_groups: str,
                 force: bool, prjct_nm: str, qiime_env: str):
 
     def run_multi_deicode(rt, out_sh, out_pbs, tsv, meta_pd,
-                          case, case_var, case_vals,
                           force, prjct_nm, qiime_env):
+
         written = 0
         with open(out_sh, 'w') as sh:
-            qza = '%s.qza' % splitext(tsv)[0]
-            cur_rad = '/'.join([rt, basename(tsv).replace('.tsv', '_%s' % case)])
-            new_tsv = '%s.tsv' % cur_rad
-            new_meta = new_tsv.replace('/tab_', '/meta_')
-            new_mat_qza = '%s_DM.qza' % cur_rad
-            new_qza = '%s.qza' % cur_rad
-            ordi_qza = '%s_deicode_ordination.qza' % cur_rad
-            ordi_qzv = '%s_deicode_ordination_biplot.qzv' % cur_rad
 
-            if force or not isfile(ordi_qzv):
-                if 'ALL' in case:
-                    new_meta_pd = meta_pd.copy()
-                elif len([x for x in case_vals if x[0]=='>' or x[0]=='<']):
-                    new_meta_pd = meta_pd.copy()
-                    for case_val in case_vals:
-                        if case_val[0] == '>':
-                            new_meta_pd = new_meta_pd[new_meta_pd[case_var] >= float(case_val[1:])].copy()
-                        elif case_val[0] == '<':
-                            new_meta_pd = new_meta_pd[new_meta_pd[case_var] <= float(case_val[1:])].copy()
-                else:
-                    new_meta_pd = meta_pd[meta_pd[case_var].isin(case_vals)].copy()
-                new_meta_pd.reset_index().to_csv(new_meta, index=False, sep='\t')
+            for case_var, case_vals_list in cases_dict.items():
+                for case_vals in case_vals_list:
+                    if len(case_vals):
+                        case = '%s_%s' % (
+                            case_var, '-'.join([x.replace('<', 'below').replace('>', 'above') for x in case_vals]))
+                    else:
+                        case = case_var
 
-                cmd = 'qiime feature-table filter-samples \\ \n'
-                cmd += '--i-table %s \\ \n' % qza
-                cmd += '--m-metadata-file %s \\ \n' % new_meta
-                cmd += '--o-filtered-table %s \\ \n' % new_qza
-                sh.write('echo "%s"\n' % cmd)
-                sh.write('%s\n' % cmd)
+                    qza = '%s.qza' % splitext(tsv)[0]
+                    cur_rad = '/'.join([rt, basename(tsv).replace('.tsv', '_%s' % case)])
+                    new_tsv = '%s.tsv' % cur_rad
+                    new_meta = new_tsv.replace('/tab_', '/meta_')
+                    new_mat_qza = '%s_DM.qza' % cur_rad
+                    new_qza = '%s.qza' % cur_rad
+                    ordi_qza = '%s_deicode_ordination.qza' % cur_rad
+                    ordi_qzv = '%s_deicode_ordination_biplot.qzv' % cur_rad
 
-                cmd = 'qiime deicode rpca \\ \n'
-                cmd += '--i-table %s \\ \n' % new_qza
-                cmd += '--p-min-feature-count 10 \\ \n'
-                cmd += '--p-min-sample-count 500 \\ \n'
-                cmd += '--o-biplot %s \\ \n' % ordi_qza
-                cmd += '--o-distance-matrix %s\n' % new_mat_qza
-                sh.write('echo "%s"\n' % cmd)
-                sh.write('%s\n' % cmd)
+                    if force or not isfile(ordi_qzv):
+                        if 'ALL' in case:
+                            new_meta_pd = meta_pd.copy()
+                        elif len([x for x in case_vals if x[0]=='>' or x[0]=='<']):
+                            new_meta_pd = meta_pd.copy()
+                            for case_val in case_vals:
+                                if case_val[0] == '>':
+                                    new_meta_pd = new_meta_pd[new_meta_pd[case_var] >= float(case_val[1:])].copy()
+                                elif case_val[0] == '<':
+                                    new_meta_pd = new_meta_pd[new_meta_pd[case_var] <= float(case_val[1:])].copy()
+                        else:
+                            new_meta_pd = meta_pd[meta_pd[case_var].isin(case_vals)].copy()
+                        new_meta_pd.reset_index().to_csv(new_meta, index=False, sep='\t')
 
-                cmd = 'qiime emperor biplot \\ \n'
-                cmd += '--i-biplot %s \\ \n' % ordi_qza
-                cmd += '--m-sample-metadata-file %s \\ \n' % new_meta
-                cmd += '--o-visualization %s \\ \n' % ordi_qzv
-                cmd += '--p-number-of-features 20\n'
-                sh.write('echo "%s"\n' % cmd)
-                sh.write('%s\n' % cmd)
-                written += 1
+                        cmd = 'qiime feature-table filter-samples \\ \n'
+                        cmd += '--i-table %s \\ \n' % qza
+                        cmd += '--m-metadata-file %s \\ \n' % new_meta
+                        cmd += '--o-filtered-table %s \\ \n' % new_qza
+                        sh.write('echo "%s"\n' % cmd)
+                        sh.write('%s\n' % cmd)
+
+                        cmd = 'qiime deicode rpca \\ \n'
+                        cmd += '--i-table %s \\ \n' % new_qza
+                        cmd += '--p-min-feature-count 10 \\ \n'
+                        cmd += '--p-min-sample-count 500 \\ \n'
+                        cmd += '--o-biplot %s \\ \n' % ordi_qza
+                        cmd += '--o-distance-matrix %s\n' % new_mat_qza
+                        sh.write('echo "%s"\n' % cmd)
+                        sh.write('%s\n' % cmd)
+
+                        cmd = 'qiime emperor biplot \\ \n'
+                        cmd += '--i-biplot %s \\ \n' % ordi_qza
+                        cmd += '--m-sample-metadata-file %s \\ \n' % new_meta
+                        cmd += '--o-visualization %s \\ \n' % ordi_qzv
+                        cmd += '--p-number-of-features 20\n'
+                        sh.write('echo "%s"\n' % cmd)
+                        sh.write('%s\n' % cmd)
+                        written += 1
 
         if written:
             xpbs_call(out_sh, out_pbs, '%s.dcd.%s' % (prjct_nm, case), qiime_env,
-                      '2', '1', '1', '2', 'gb')
+                      '2', '1', '1', '200', 'mb')
         else:
             os.remove(out_sh)
 
@@ -91,34 +100,27 @@ def run_deicode(i_folder: str, datasets: dict, p_perm_groups: str,
     jobs = []
     all_shs = []
     main_sh = '%s/3_run_beta_deicode.sh' % job_folder
-    with open(main_sh, 'w') as o:
+    with open(main_sh, 'w') as main_o:
         for dat, tsv_meta_pds in datasets.items():
             tsv, meta = tsv_meta_pds
             rt = get_analysis_folder(i_folder, 'deicode/%s' % dat)
             # meta_pd = pd.read_csv(meta, header=0, index_col=0, sep='\t', dtype=str)
             meta_pd = pd.read_csv(meta, header=0, index_col=0, sep='\t')
             cases_dict = check_metadata_cases_dict(meta, meta_pd, dict(main_cases_dict))
-            for case_var, case_vals_list in cases_dict.items():
-                for case_vals in case_vals_list:
-                    if len(case_vals):
-                        case = '%s_%s' % (
-                            case_var, '-'.join([x.replace('<', 'below').replace('>', 'above') for x in case_vals]))
-                    else:
-                        case = case_var
-                    out_sh = '%s/run_beta_deicode_%s.sh' % (job_folder2, case)
-                    all_shs.append(out_sh)
-                    out_pbs = out_sh.replace('.sh', '.pbs')
-                    o.write('qsub %s\n' % out_pbs)
-                    p = multiprocessing.Process(
-                        target=run_multi_deicode,
-                        args=(
-                            rt, out_sh, out_pbs, tsv, meta_pd,
-                            case, case_var, case_vals,
-                            force, prjct_nm, qiime_env,
-                        )
-                    )
-                    p.start()
-                    jobs.append(p)
+
+            out_sh = '%s/run_beta_deicode_%s.sh' % (job_folder2, dat)
+            all_shs.append(out_sh)
+            out_pbs = out_sh.replace('.sh', '.pbs')
+            main_o.write('qsub %s\n' % out_pbs)
+            p = multiprocessing.Process(
+                target=run_multi_deicode,
+                args=(
+                    rt, out_sh, out_pbs, tsv, meta_pd,
+                    force, prjct_nm, qiime_env,
+                )
+            )
+            p.start()
+            jobs.append(p)
     for j in jobs:
         j.join()
 
