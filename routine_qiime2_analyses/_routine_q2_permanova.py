80c65e016d0cccc0d85776fdd8274a293536da85
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-19 12:51:32 -0800

    fix bug #2

--- routine_qiime2_analyses/_routine_q2_permanova.py
+++ routine_qiime2_analyses/_routine_q2_permanova.py
@@ -126,7 +126,7 @@ def run_permanova(i_datasets_folder: str, datasets: dict, betas: dict, main_test
                     for testing_group in testing_groups_case_var:
                         if testing_group == 'ALL':
                             continue
-                        cur_sh = '%s/run_beta_group_significance_%s_%s_%s-%s_%s.sh' % (
+                        cur_sh = '%s/run_beta_group_significance_%s_%s_%s.sh' % (
                             job_folder2, dat, case_, testing_group)
                         print('cur_sh:', cur_sh)
                         cur_sh = cur_sh.replace(' ', '-')
