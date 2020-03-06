4829b93759bf8ad329e9189ce15c5324e8a9537f
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-03-05 22:02:08 -0800

    solve taxonomy removal bug

--- routine_qiime2_analyses/_routine_q2_taxonomy.py
+++ routine_qiime2_analyses/_routine_q2_taxonomy.py
@@ -146,7 +146,7 @@ def run_taxonomy(i_datasets_folder: str, datasets_read: dict, datasets_phylo: di
                         cmd = run_taxonomy_wol(force, tsv_pd, out_qza, out_tsv,
                                                cur_datasets_features)
                     else:
-                        if len(tsv_pd.index.unique()) == tsv_pd.shape[0]:
+                        if len([x for x in tsv_pd.index if str(x).isdigit()]) == tsv_pd.shape[0]:
                             continue
                         taxonomies[dat] = ['feat', out_qza]
                         cmd = run_taxonomy_others(force, tsv_pd, out_qza, out_tsv)
