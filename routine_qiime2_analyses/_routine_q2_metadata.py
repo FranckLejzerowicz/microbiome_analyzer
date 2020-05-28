abb3732f494ffadf2579dbec790ef9421dd3255e
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-05-27 18:21:17 -0700

    mmbrd

--- routine_qiime2_analyses/_routine_q2_metadata.py
+++ routine_qiime2_analyses/_routine_q2_metadata.py
@@ -100,13 +100,9 @@ def check_metadata_models(meta: str, meta_pd: pd.DataFrame,
                 ', '.join(sorted(only_formula)), model, formula))
             continue
         if len(levels):
-            levels_set = set(meta_pd[formula_split[0]].unique())
+            levels_set = [str(x) for x in meta_pd[formula_split[0]].unique()]
             if 'Diff' in formula:
                 if not set(levels).issubset(levels_set):
-                    print('levels')
-                    print(levels)
-                    print('levels_set')
-                    print(levels_set)
                     not_in_levels = set(levels_set).difference(levels)
                     print('Songbird formula factors(s) missing in metadata "%s":\n  %s' % (
                         formula_split[0], not_in_levels))
