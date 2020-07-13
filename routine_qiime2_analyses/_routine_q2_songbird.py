6bfd31f5ac37c4f681f5af08400a4b0420a5c522
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-07-12 19:43:04 -0700

    sngbrd

--- routine_qiime2_analyses/_routine_q2_songbird.py
+++ routine_qiime2_analyses/_routine_q2_songbird.py
@@ -260,9 +260,8 @@ def run_songbird(p_diff_models: str, i_datasets_folder: str, datasets: dict,
                                 epoch, diff_prior, thresh_feat,
                                 thresh_sample, n_random, baselines, baseline
                             )
-                            if pair:
-                                songbird_outputs.append([dat, filt, params.replace('/', '__'),
-                                                         case, diffs, pair])
+                            songbird_outputs.append([dat, filt, params.replace('/', '__'),
+                                                     case, diffs, pair])
 
     job_folder = get_job_folder(i_datasets_folder, 'songbird')
     main_sh = write_main_sh(job_folder, '2_songbird%s' % filt_raref, all_sh_pbs,
