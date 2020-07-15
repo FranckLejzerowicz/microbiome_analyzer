be6231ca481c8a4b3da9e9a0a23f2365ff345e13
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-07-15 09:00:30 -0700

    mmbrd

--- routine_qiime2_analyses/_routine_q2_mmbird.py
+++ routine_qiime2_analyses/_routine_q2_mmbird.py
@@ -476,6 +476,7 @@ def get_omics_songbirds_taxa(i_datasets_folder, mmvec_songbird_pd, taxo_pds):
             drop_columns = [col for col in omic_songbird_ranks.columns if omic_songbird_ranks[col].unique().size == 1]
             meta_omic_pd = omic_songbird_ranks.drop(columns=drop_columns)
             meta_omic_pd.to_csv(meta_omic_fp, index=False, sep='\t')
+            print('<< written: %s >>' % meta_omic_fp)
             meta_omic_pd.set_index('Feature ID', inplace=True)
             omics_pairs_metas[(pair, omic, filt)] = (meta_omic_fp, meta_omic_pd, feats_diff_cols)
     return omics_pairs_metas
