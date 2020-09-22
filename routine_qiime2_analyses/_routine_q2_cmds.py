be9e1c0bda36cd2b4eb2d6bd1bc2c8c5534ad2f4
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-09-21 23:00:23 -0700

    permanova as type str

--- routine_qiime2_analyses/_routine_q2_cmds.py
+++ routine_qiime2_analyses/_routine_q2_cmds.py
@@ -918,7 +918,7 @@ def add_q2_types_to_meta(new_meta_pd: pd.DataFrame, new_meta: str, testing_group
     new_meta_pd = pd.concat([q2types, new_meta_pd]).reset_index()
     new_meta_pd = new_meta_pd.replace({testing_group: dict(
         (x, x.replace('(', '').replace(')', '').replace('/', ''))
-        for x in new_meta_pd[testing_group].unique()
+        for x in new_meta_pd[testing_group].astype(str).unique()
         if str(x) != 'nan' and x != x.replace('(', '').replace(')', '').replace('/', '')
     )})
     new_meta_pd[[col_index, testing_group]].to_csv(new_meta, index=False, sep='\t')
