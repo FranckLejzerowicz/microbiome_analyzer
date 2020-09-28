10d0fc45f01c9548dbe3394735a008e4bb36e4e5
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-09-28 09:24:43 -0700

    sb

--- routine_qiime2_analyses/_routine_q2_songbird.py
+++ routine_qiime2_analyses/_routine_q2_songbird.py
@@ -163,7 +163,7 @@ def get_songbird_metadata_train_test(meta_pd, meta_vars_, meta_var, new_meta,
         new_meta_pd = new_meta_pd.loc[(~new_meta_pd[meta_var.lower()].isin(drop)), :]
     new_meta_pd_ = new_meta_pd.copy()
 
-    new_meta_pd_['tmptmptmp'] = [''.join(x) for x in new_meta_pd_.values if str(x) != 'nan']
+    new_meta_pd_['tmptmptmp'] = [''.join(map(str, x)) for x in new_meta_pd_.values if str(x) != 'nan']
     if 1 in new_meta_pd_.tmptmptmp.value_counts():
         return None
 
