c0c65d8b21b674067f4ec897aa33a10884d2b0d1
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-09-29 16:30:00 -0700

    sb

--- routine_qiime2_analyses/_routine_q2_io_utils.py
+++ routine_qiime2_analyses/_routine_q2_io_utils.py
@@ -526,6 +526,7 @@ def write_main_sh(job_folder: str, analysis: str, all_sh_pbs: dict,
                                 sh.write(line)
                                 cur_written = True
                         os.remove(cur_sh)
+            continue
             if cur_written:
                 out_pbs = '%s.pbs' % splitext(out_sh)[0]
                 run_xpbs(out_sh, out_pbs, '%s.%s' % (prjct_nm, dat), qiime_env,
