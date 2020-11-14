b2d029b6fb479e9ca1a93cfaff0811fc856f3d09
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-11-13 17:35:16 -0800

    fix split_taxa_pds

--- routine_qiime2_analyses/_routine_q2_taxonomy.py
+++ routine_qiime2_analyses/_routine_q2_taxonomy.py
@@ -114,6 +114,8 @@ def get_taxo_levels(taxonomies: dict) -> dict:
     split_taxa_pds = {}
     for dat, tax_fp in taxonomies.items():
         rewrite = False
+        if not isfile(tax_fp[-1]):
+            continue
         tax_pd = pd.read_csv(tax_fp[-1], header=0, sep='\t', dtype=str)
         tax_pd.rename(columns={tax_pd.columns[0]: 'Feature ID'}, inplace=True)
         features = tax_pd['Feature ID'].tolist()
