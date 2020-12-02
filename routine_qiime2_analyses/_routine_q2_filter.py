1656aded63aa7f0e2f1b369fb8d6796c5e125143
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-12-02 14:29:34 -0800

    import

--- routine_qiime2_analyses/_routine_q2_filter.py
+++ routine_qiime2_analyses/_routine_q2_filter.py
@@ -65,7 +65,6 @@ def import_datasets(i_datasets_folder: str, datasets: dict, datasets_phylo: dict
                         cur_sh.write('%s\n' % cmd)
                         written += 1
             if written:
-                print(out_sh)
                 main_written += 1
                 to_chunk.append(out_sh)
                 if not chunkit:
@@ -74,6 +73,7 @@ def import_datasets(i_datasets_folder: str, datasets: dict, datasets_phylo: dict
                              run_params["mem_num"], run_params["mem_dim"],
                              chmod, written, 'single', o, noloc, jobs)
     if to_chunk:
+        print(to_chunk)
         simple_chunks(run_pbs, job_folder2, to_chunk, 'imports',
                       prjct_nm, run_params["time"], run_params["n_nodes"], run_params["n_procs"],
                       run_params["mem_num"], run_params["mem_dim"],
