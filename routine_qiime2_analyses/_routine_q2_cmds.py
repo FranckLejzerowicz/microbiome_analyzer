b27fbff67d5a0fd81573f142af75dd3bf6b4a7d2
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-12-15 13:51:11 -0800

    feature subsets for numeric ids

--- routine_qiime2_analyses/_routine_q2_cmds.py
+++ routine_qiime2_analyses/_routine_q2_cmds.py
@@ -6,7 +6,6 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import re
 import pandas as pd
 from typing import TextIO
 from os.path import dirname, isdir, isfile, splitext
@@ -24,7 +23,10 @@ def get_subset(tsv_pd: pd.DataFrame, subset_regex: list) -> list:
     """
     to_keep_feats = {}
     for regex in subset_regex:
-        to_keep_feats[regex.lower()] = tsv_pd.index.str.lower().str.contains(regex.lower())
+        if regex.isdigit():
+            to_keep_feats[regex] = tsv_pd.index.str.contains(regex)
+        else:
+            to_keep_feats[regex.lower()] = tsv_pd.index.str.lower().str.contains(regex.lower())
     to_keep_feats_pd = pd.DataFrame(to_keep_feats)
     to_keep_feats = to_keep_feats_pd.any(axis=1)
     feats_subset_list = tsv_pd.index[to_keep_feats].tolist()
