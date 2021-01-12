e2780fa1419bf6211c63dd88a0c3c4adea7792a3
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2021-01-11 18:55:16 -0800

    check dataset with mixed ID dtypes

--- routine_qiime2_analyses/_routine_q2_io_utils.py
+++ routine_qiime2_analyses/_routine_q2_io_utils.py
@@ -857,7 +857,8 @@ def get_datasets(i_datasets: tuple, i_datasets_folder: str) -> (dict, dict, dict
             path_pd = load_table(path).to_dataframe()
         else:
             path_pd = read_meta_pd(path, '#OTU ID')
-            #
+            print('----')
+            print(path_pd)
             # tab_feat_col = get_feature_sample_col(path)
             # path_pd = pd.read_csv(path, header=0, index_col=0, sep='\t')
         meta_sam_col = get_feature_sample_col(meta)
