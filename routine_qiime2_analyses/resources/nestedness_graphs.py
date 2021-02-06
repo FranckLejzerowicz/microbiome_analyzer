88ede781d6bcbc6c619f979b8ad4f1b3f3c6806a
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2021-02-05 16:43:49 -0800

    no summary "metadata" graph for nestedness figure

--- routine_qiime2_analyses/resources/nestedness_graphs.py
+++ routine_qiime2_analyses/resources/nestedness_graphs.py
@@ -10,10 +10,6 @@ import seaborn as sns
 
 
 dat = '<DAT>'
-if '_tx-' in dat:
-    taxo_level = dat.split('_tx-')[-1]
-else:
-    taxo_level = 'Features'
 cur_raref = '<CUR_RAREF>'
 tab_pd = pd.read_table('<TAB_FP>', index_col=0)
 if tab_pd.shape[0] > 1000:
@@ -218,13 +214,13 @@ else:
                         axes.legend(handles=hands, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
                         axes.set_title(col)
                         axes.set_xlabel('Samples (sorted by richness)')
-                        axes.set_ylabel('%s (sorted by prevalence)' % taxo_level)
+                        axes.set_ylabel('%s (sorted by prevalence)' % level)
                     else:
                         axes[cdx].imshow(mat, aspect="auto")
                         axes[cdx].legend(handles=hands, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
                         axes[cdx].set_title(col)
                         axes[cdx].set_xlabel('Samples (sorted by richness)')
-                        axes[cdx].set_ylabel('%s (sorted by prevalence)' % taxo_level)
+                        axes[cdx].set_ylabel('%s (sorted by prevalence)' % level)
                 suptitle = '%s%s (%s coloring)' % (dat, cur_raref, typ)
                 top = .93
                 if case != 'ALL':
