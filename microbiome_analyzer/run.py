c00d640904fe454329f2c71f9a9a828fd108b06b
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-10-06 11:59:56 +0200

    fix typo

--- microbiome_analyzer/run.py
+++ microbiome_analyzer/run.py
@@ -35,7 +35,7 @@ def runner(**kwargs):
     if config.filt3d:
         analysis.explore_filtering()
     analysis.filter()
-    if config.rarefactions:
+    if config.rarefy:
         analysis.rarefy()
 
     # if config.qemistree and 'qemistree' not in config.skip:
