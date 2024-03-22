118b4485e0e0ac53c1379b56225e94d0e19db301
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2024-03-22 16:52:41 +0100

    major update to incl. CTF / Volatility (incl. of CTF sample trajectories); refine config-making and move permanova and time/subject info as config files; use one-folder-per-dataset as data/metadata subfolders

--- microbiome_analyzer/__init__.py
+++ microbiome_analyzer/__init__.py
@@ -6,4 +6,4 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "3.2"
+__version__ = "4.0"
